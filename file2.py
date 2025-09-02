0a4814a667a3d2ca1cdd88421840119326380094
soheil <258soheil@gmail.com>
2025-09-02 11:11:53 -0700

    Revert "commit master revert 4"
    This reverts commit 81828b915efa93074bdf5ff6e5148a5af6f837aa.

--- file2.py
+++ file2.py
@@ -37,11 +37,15 @@
 #bbb
 #ttt
 <<<<<<< HEAD
+<<<<<<< HEAD
 #vvv
 #rrr
 =======
 #bbb
 >>>>>>> parent of 9219de5 (commit master revert 2)
+=======
+#vvv
+>>>>>>> parent of 81828b9 (commit master revert 4)
 =======
 #vvv
 >>>>>>> parent of 81828b9 (commit master revert 4)
