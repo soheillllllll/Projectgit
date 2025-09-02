f967e76fb1975d8aaf6cfbe7b66f4a0955148055
soheil <258soheil@gmail.com>
2025-09-02 11:14:18 -0700

    commi

--- file2.py
+++ file2.py
@@ -33,19 +33,6 @@
 #mju
 #nhyt
 #uy
-<<<<<<< HEAD
 #bbb
 #ttt
-<<<<<<< HEAD
-<<<<<<< HEAD
-#vvv
-#rrr
-=======
-#bbb
->>>>>>> parent of 9219de5 (commit master revert 2)
-=======
-#vvv
->>>>>>> parent of 81828b9 (commit master revert 4)
-=======
-#vvv
->>>>>>> parent of 81828b9 (commit master revert 4)
+
